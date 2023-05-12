e1f8fa2fe5b1efcb205d7b104cd00138414c5e31
Karine Pereira <karinedeveloper@gmail.com>
2023-05-12 11:21:37 -0300

    adicionado README.md

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,6 @@
 const btn = document.getElementById("botao-alterar-tema");
 const body = document.getElementById("body");
 
-
-btn.addEventListener("click",() =>{
-    body.classList.toggle("modo-escuro")
-})
+btn.addEventListener("click", () => {
+  body.classList.toggle("modo-escuro");
+});
